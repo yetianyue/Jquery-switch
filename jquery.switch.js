55b12dd2ddba6f6dad904de0c967d41601d8fa63
yetianyue <646682485@qq.com>
2018-01-12 10:31:12 +0800

    解决动动画问题

--- jquery.switch.js
+++ jquery.switch.js
@@ -18,20 +18,23 @@ $.extend({
         if (switchs.length <= 0) {
             return false
         }
-        var divs = '<div class="switch-button"></div>'
+        var divs = '<div class="switch-button"></div><div class="switch-bg"></div>'
         switchs.html(divs)
         setStyle()
 
-        var btns = switchs.find('div')
+        var btns = switchs.find('.switch-button')
 
         $.each(btns,function (n,m) {
             var that = $(m)
             var parentObj = that.parent()
+            var nextObj = that.next()
             var className = parentObj.attr('class')
             if (options.height !=100) {
                 parentObj.css('width',options.height)
                 parentObj.css('height',options.height/2)
                 parentObj.css('border-radius',options.height)
+                nextObj.css('height',options.height/2)
+                nextObj.css('border-radius',options.height)
                 that.css('width',options.height/2)
                 that.css('height',options.height/2)
                 that.css('border-radius',options.height/2)
@@ -43,22 +46,27 @@ $.extend({
 
             if(className.indexOf('switch-on') > 0){
                 that.animate({left:options.height/2+'px'},options.speed)
+                nextObj.animate({width:options.height+'px'},options.speed)
             }
         })
 
         var swAnimate = function (that) {
-            var son = that.find('div')
+            var son = that.find('.switch-button'), sonbg = that.find('.switch-bg')
             var className = that.attr('class')
             var newClassName
             if (className.indexOf('switch-on') > 0 ) {
                 son.animate({left:'0px'},options.speed)
+                sonbg.animate({width:'0px'},options.speed)
                 newClassName = className.replace(/switch-on/,'switch-off')
             }else{
                 son.animate({left:options.height/2+'px'},options.speed)
+                sonbg.animate({width:options.height+'px'},options.speed)
                 if (className.indexOf('switch-off') < 0) {
                     newClassName = className+" switch-on";
+                }else{
+                    newClassName =  className.replace(/switch-off/,'switch-on')
                 }
-                newClassName =  className.replace(/switch-off/,'switch-on')
+                
             }
             that.attr('class',newClassName)
         }
@@ -83,7 +91,8 @@ $.extend({
                     height: '50px',
                     'border-radius': '50px',
                     border:'1px solid rgba(0,0,0,0.15)',
-                    position: 'relative'
+                    position: 'relative',
+                    'overflow':'hidden'
                 },
                 'switch-button':{
                     width: "50px",
@@ -91,14 +100,17 @@ $.extend({
                     'border-radius': "50px",
                     position: "absolute",
                     background: "white",
+                    'z-index':'2',
                     'box-shadow': "0px 2px 4px rgba(0,0,0,0.4)",
                     cursor: "pointer"
                 },
-                'switch-on':{
+                'switch-bg':{
+                    width: '0px',
+                    height: '50px',
+                    'border-radius': '50px',
+                    position: 'absolute',
+                    'z-index':'1',
                     background: "rgba(0,184,0,0.8)"
-                },
-                'switch-off':{
-                    background: "rgba(255,255,255,0.4)"
                 }
             }
             if ($.switchStyle === false) {
